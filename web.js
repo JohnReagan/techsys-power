bffb569d43c8f76b490f3b7e10fb58ac63dd2027
jtr4fh <jtr4fh@virginia.edu>
2014-06-15 05:22:57 -0400

    more heroku bugs

--- web.js
+++ web.js
@@ -4,27 +4,20 @@ var path = require('path');
 var app = express();
 var logfmt = require('logfmt');
 
-//require('./routes')(app);
+
 app.use(logfmt.requestLogger());
 app.use(express.static(path.normalize(__dirname) + '/app'));
 app.use('/bower_components',express.static(path.normalize(__dirname + '/bower_components')));
 
-/*
-app.configure(function() {
-	//app.use(express.logger('dev'));
-	app.use(express.bodyParser());
-});*/
-
-var port = Number(process.env.PORT || 5000);
-
-app.set('views', path.normalize(__dirname) + '/app');
-app.set('view engine', 'html');
-app.engine('html', require('ejs').renderFile);
+// app.set('views', path.normalize(__dirname) + '/app');
+// app.set('view engine', 'html');
 
 var port = Number(process.env.PORT || 8000);
+
 app.get('/', function(req, res) {
-		res.sendfile('./app/index.html'); // load the single view file (angular will handle the page changes on the front-end)
-	});
+	res.sendfile('./app/index.html'); // load the single view file (angular will handle the page changes on the front-end)
+});
+
 app.listen(port);
 
 console.log("Express server listening to port " + port);
