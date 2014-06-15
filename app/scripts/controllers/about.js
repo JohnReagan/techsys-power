8e92974f3815a830fca2b7600a289c42ce59afa5
jtr4fh <jtr4fh@virginia.edu>
2014-06-14 23:03:34 -0400

    inital commit yo scaffolding

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name techsysPowerApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the techsysPowerApp
+ */
+angular.module('techsysPowerApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
